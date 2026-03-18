f21aa601c30dbaf1b5ebdd2b85385d8c8e6e0349
shirley6126 <shirley.daya1406@gmail.com>
2026-03-18 10:17:17 -0500

    codigo resta

---
+++ Lista Doblemente Enlazada/resta.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, resultado;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    resultado = num1 - num2;
+
+    cout << "La resta es: " << resultado << endl;
+
+    return 0;
+}
